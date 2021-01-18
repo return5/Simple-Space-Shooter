6d4f89f92b8bb5bef43a033df0e03b923834f00b
return5 <armoredmoose@gmail.com>
2021-01-18 01:55:19 -0500

    added ui and comments

--- main.lua
+++ main.lua
@@ -23,6 +23,11 @@ local Enemy_ship  = require("aux_files.enemy_ship")
 local Player_ship = require("aux_files.player_ship")
 local Background  = require("aux_files.background")
 
+local function printUI()
+    love.graphics.print("Player health: " .. PLAYER.health,1,1)
+    love.graphics.print("Player Score: " .. PLAYER_SCORE,1,15)
+end
+
 function love.draw()
     love.graphics.push()
     love.graphics.translate(-PLAYER.x + HALF_W, -PLAYER.y + HALF_H)
@@ -32,8 +37,10 @@ function love.draw()
     iterateList(ENEMY_PROJ,printProj)
     PLAYER:printPlayer()
     love.graphics.pop()
+    printUI()
 end
 
+--get the x and y of mouse pointer
 function playerTargetMouse()
     PLAYER.target_x = love.mouse.getX() + PLAYER.x - HALF_W
     PLAYER.target_y = love.mouse.getY() + PLAYER.y - HALF_H
@@ -44,43 +51,42 @@ function love.keypressed(_,scancode,_)
 end
 
 function love.mousepressed(x,y,button,_,_)
-    if button == 1 then
+    if button == 1 then  --if player pressed left click then shoot a projectile
         PLAYER:shootFunc()
     end
 
 end
 
 function love.update(dt)
-    iterateList(SHIP_LIST,updateEnemyShip,dt)
-    iterateList(PLAYER_PROJ,updateProjectile,dt)
-    iterateList(ENEMY_PROJ,updateProjectile,dt)
-    if love.keyboard.isScancodeDown('w') then
+    iterateList(SHIP_LIST,updateEnemyShip,dt)    --loop over all enemy ships and update each one
+    iterateList(PLAYER_PROJ,updateProjectile,dt) --loop over all player projectiles and update each one
+    iterateList(ENEMY_PROJ,updateProjectile,dt)  --loop over each enemy projectile and update each one
+    if love.keyboard.isScancodeDown('w') then    -- if player is pressing the 'w' key
         MOVE = true
     else
         MOVE = false
     end
-
     PLAYER:updatePlayer(dt)
 end
 
 function love.load()
     math.randomseed(os.time())
-    WINDOW_W      = 900 
-    WINDOW_H      = 900
-    HALF_W        = WINDOW_W / 2
-    HALF_H        = WINDOW_H / 2
-    GAME_W        = 4000 
-    GAME_H        = 4000 
-    love.window.setMode(WINDOW_W,WINDOW_H)
-    BG_CANVAS     = makeBackground()
-    SHIP_LIST     = {}
-    PLAYER_PROJ   = {}
-    ENEMY_PROJ    = {}
-    PLAYER        = PLAYER_SHIP:makePlayer()
-    makeEnemyShips()
-    PLAY          = true
-    MOVE          = false
-    PLAYER_SCORE  = 0
-    love.keyboard.setKeyRepeat(true)
+    WINDOW_W      = 900            --size of window width
+    WINDOW_H      = 900            --size of window height
+    HALF_W        = WINDOW_W / 2   --half of window width
+    HALF_H        = WINDOW_H / 2   --half of window height
+    GAME_W        = 4000           --width of game map
+    GAME_H        = 4000           --height of game map 
+    love.window.setMode(WINDOW_W,WINDOW_H)  --set size of window
+    BG_CANVAS     = makeBackground()        -- make the background canvas
+    SHIP_LIST     = {}  --list of all enemy ships
+    PLAYER_PROJ   = {}  --list of projectiles shot by player
+    ENEMY_PROJ    = {}  --list of projectiles shot by enemy
+    PLAYER        = PLAYER_SHIP:makePlayer()  
+    makeEnemyShips()  
+    PLAY          = true    --should game keep going
+    MOVE          = false   --is player moving
+    PLAYER_SCORE  = 0       --player's score
+    love.keyboard.setKeyRepeat(true)  --allow player to hold down a key 
    
 end
